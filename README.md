08f63233c5c78971e9cafcc34783d15df332c9e1
Sai Vineet <saivineet89@gmail.com>
2014-06-01 15:14:59 +0530

    Release!

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ SublimePaster
 =============
 
 A Paste plugin for Sublime, to create pastes quickly.
+
+Installation
+------------
+
+Support for ST3 only for now.
+
+Just download the `SublimePaster.sublime-package` file and put it in your sublime text's `Packages` directory, and it'll work.
+
+You can also `git clone` the source in the same directory and it'll work.
--- SublimePaster.py
+++ SublimePaster.py
@@ -15,29 +15,38 @@ class PasterCommand(sublime_plugin.TextCommand):
             content += self.view.substr(sel)
 
         sublime.status_message("Creating Paste!")
-        
+
+        name = self.view.file_name().split("/")[-1]
         syntax = self.view.settings().get('syntax')
         syntax = syntax.split("/")[-1].split(".")[0]
         print (syntax)
 
-        self.paste(content, syntax)
+        self.paste(content, name, syntax)
 
-    def paste(self, content, syntax):
+    def paste(self, content, name, syntax):
         url = 'http://pastebin.com/api/api_post.php'
         values = {
                   'api_dev_key': API_KEY,
                   'api_paste_code': content,
                   'api_option': 'paste',
-                  # 'api_paste_format': syntax
+                  'api_paste_format': syntax
             }
+        if name is not None: values['api_paste_name'] = name
 
         data = urllib.parse.urlencode(values).encode('utf-8')
         req = urllib.request.Request(url, data)
         try:
             response = urllib.request.urlopen(req)
-            paste_url = response.read().decode('utf-8')
         except urllib.error.URLError as e:
-            pass
+            if hasattr(e, 'reason'):
+                sublime.error_message(
+                    'SublimePaster failed to reach a server, reason: '+e.reason)
+            elif hasattr(e, 'code'):
+                sublime.error_message(
+                    'Server sent an error: '+e.code)
+        else:
+            paste_url = response.read().decode('utf-8')
+            sublime.set_clipboard(paste_url)
+            sublime.status_message("Paste link copied to Clipboard")
 
-        sublime.set_clipboard(paste_url)
-        sublime.message_dialog("Paste link copied to Clipboard")
+        
